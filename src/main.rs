626f47c494f7595fe3d64dc8d4cb1b6be33819e6
Gibson Fahnestock <gibfahn@gmail.com>
2018-07-27 11:48:28 +0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+#![feature(rust_2018_preview)]
+
+#[macro_use] extern crate quicli;
+use quicli::prelude::*;
+
+// Add cool slogan for your app here, e.g.:
+/// Get first n lines of a file
+#[derive(Debug, StructOpt)]
+struct Cli {
+}
+
+fn main() {}
