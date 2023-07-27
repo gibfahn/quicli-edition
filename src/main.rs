f716077334064eea9267c3a41f7a05e3d3868374
Gibson Fahnestock <gibfahn@gmail.com>
2023-07-27 23:30:35 +0100

    Update to 2021 edition, issue still reproduces
    ```console
    ❯ cargo --version
    cargo 1.71.0 (cfd3bbd8f 2023-06-08)
    
    ❯ cargo build
       Compiling quicli-edition v0.1.0 (/Users/gib/code/tmp/quicli-edition)
    error[E0433]: failed to resolve: could not find `structopt` in the list of imported crates
     --> src/main.rs:4:17
      |
    4 | #[derive(Debug, StructOpt)]
      |                 ^^^^^^^^^ could not find `structopt` in the list of imported crates
      |
      = note: this error originates in the derive macro `StructOpt` (in Nightly builds, run with -Z macro-backtrace for more info)
    
    For more information about this error, try `rustc --explain E0433`.
    error: could not compile `quicli-edition` (bin "quicli-edition") due to previous error
    ```

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
-#![feature(rust_2018_preview)]
-#![feature(proc_macro_path_invoc)]
-#![warn(rust_2018_idioms)]
-
 use quicli::prelude::structopt::StructOpt;
 
 /// Get first n lines of a file
 #[derive(Debug, StructOpt)]
-struct Cli {
-}
+struct Cli {}
 
 fn main() {}
